824a4b5a51c1deffd08468a854d5d074e48c08e4
yangfukai <yangfukai@zhonggu.cn>
2018-03-22 17:48:09 +0800

    test

--- springboot-dubbo-consumer01/src/main/java/com/yangkai/controller/TestController.java
+++ springboot-dubbo-consumer01/src/main/java/com/yangkai/controller/TestController.java
@@ -4,6 +4,7 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.yangkai.service.TestService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,12 +12,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author yangkai on 2018/3/22.
  */
 @Controller
+@RequestMapping("/test")
 public class TestController {
 
     @Reference(version = "0.0.1",check = false)
     private TestService testService;
 
-    @GetMapping()
+    @GetMapping("/mytest")
     public @ResponseBody String test(@RequestParam("data") String data){
         testService.test(data);
         return data;
--- springboot-dubbo-consumer01/src/main/java/com/yangkai/springbootdubboconsumer01/DubboConsumer01Application.java
+++ springboot-dubbo-consumer01/src/main/java/com/yangkai/springbootdubboconsumer01/DubboConsumer01Application.java
@@ -3,9 +3,11 @@ package com.yangkai.springbootdubboconsumer01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ComponentScan("com.yangkai")
+@ImportResource(value = {"classpath:consumer.xml"})
 public class DubboConsumer01Application {
 
 	public static void main(String[] args) {
