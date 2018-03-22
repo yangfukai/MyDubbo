3e93eec1b1b23ebac5b108e908436ff83013894e
yangfukai <yangfukai@zhonggu.cn>
2018-03-22 17:48:49 +0800

    test

--- springboot-dubbo-provider02/src/main/java/com/yangkai/serviceImpl/TestServiceImpl.java
+++ springboot-dubbo-provider02/src/main/java/com/yangkai/serviceImpl/TestServiceImpl.java
@@ -1,12 +1,10 @@
 package com.yangkai.serviceImpl;
 
-import com.alibaba.dubbo.config.annotation.Service;
 import com.yangkai.service.TestService;
 
 /**
  * @author yangkai on 2018/3/22.
  */
-@Service(version = "0.0.1")
 public class TestServiceImpl implements TestService {
     private static final long serialVersionUID = 3710896460707033186L;
 
